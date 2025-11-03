1a75aa3c2433cb3aa67b7c9a87be0c84ff988142
Jonikorjk <zenaserdykov2004@gmail.com>
2025-11-03 18:11:57 +0200

    Remove workflow files

--- benchmarks/storage/lib/bench_result.dart
+++
@@ -1,9 +0,0 @@
-import 'package:meta/meta.dart';
-
-@immutable
-class BenchResult {
-  final Duration time;
-  final double size;
-
-  const BenchResult({required this.time, required this.size});
-}
--- benchmarks/storage/lib/db_type.dart
+++
@@ -1,9 +0,0 @@
-enum DbType {
-  hive,
-  isar;
-
-  String boxFileName(String name) => switch (this) {
-    DbType.hive => '$name.hive',
-    DbType.isar => '$name.isar',
-  };
-}
